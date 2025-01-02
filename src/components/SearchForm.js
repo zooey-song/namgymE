6a2ebe72ecd06462e7768321caaa05cf895aeb70
zooey <minimonimo@gmail.com>
2025-01-02 17:19:33 +0900

    써치폼 셀렉트 동기화 수정

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./SearchForm.css"; // 스타일 정의
 
 const SearchForm = () => {
+
+  const [firstSelectValue, setFirstSelectValue] = useState('option1');
+  const [secondSelectOptions, setSecondSelectOptions] = useState([]);
+
+  // 첫 번째 select 값이 변경될 때마다 두 번째 select의 옵션을 업데이트
+  useEffect(() => {
+    let options;
+    if (firstSelectValue === '21') {
+      options = ['강서군', '금정구', '기장군', '남구', '동구', '동래구', '부산진구', '북구', '사상구', '사하구', '서구', '수영구', '연제구', '영도구', '중구', '해운대구'];
+    } else if (firstSelectValue === '48') {
+      options = ['거제시', '거창군', '고성군', '김해시', '남해군', '밀양시', '사천시', '산청군', '양산시', '의령군', '진주시', '창녕군', '창원시', '통영시', '하동군', '함안군', '함양군', '합천군'];
+    } else {
+      options = ['시·군·구 선택'];
+    }
+    setSecondSelectOptions(options);
+  }, [firstSelectValue]);
+
+
+
   return (
     <div className="search-form">
       <div className="form-row">
@@ -46,32 +65,15 @@ const SearchForm = () => {
       <div className="form-row">
         <div className="form-group">
           <label>희망지역</label>
-          <select className="form-control">
+          <select className="form-control" value={firstSelectValue} onChange={e => setFirstSelectValue(e.target.value)}>
+            <option value="00">시·도 선택</option>
             <option value="21">부산</option>
+            <option value="48">경상남도</option>
           </select>
           <select className="form-control">
-            <option value="">시·군·구 선택</option>
-            <option value="중구">중구</option>
-            <option value="서구">서구</option>
-            <option value="동구">동구</option>
-            <option value="영도구">영도구</option>
-            <option value="부산진구">부산진구</option>
-            <option value="동래구">동래구</option>
-            <option value="남구">남구</option>
-            <option value="북구">북구</option>
-            <option value="해운대구">해운대구</option>
-            <option value="사하구">사하구</option>
-            <option value="금정구">금정구</option>
-            <option value="강서구">강서구</option>
-            <option value="연제구">연제구</option>
-            <option value="수영구">수영구</option>
-            <option value="사상구">사상구</option>
-            <option value="기장군">기장군</option>
-            <option value="기장읍">기장읍</option>
-            <option value="장안읍">장안읍</option>
-            <option value="정관읍">정관읍</option>
-            <option value="일광면">일광면</option>
-            <option value="철마면">철마면</option>
+            {secondSelectOptions.map(option => (
+            <option key={option} value={option}>{option}</option>
+            ))}
           </select>
         </div>
         <div className="form-group">
